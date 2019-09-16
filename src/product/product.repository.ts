14b246103e8a00a430821af6bdee239675029a38
kyhsa93 <kyhsa93@naver.com>
2019-09-16 21:50:35 +0900

    Remoce legacy

--- src/product/product.repository.ts
+++
@@ -1,5 +0,0 @@
-// import { EntityRepository, Repository } from 'typeorm';
-// import { Product as ProductEntity } from './product.entity';
-
-// @EntityRepository(ProductEntity)
-// export class ProductRepository extends Repository<ProductEntity> {}
