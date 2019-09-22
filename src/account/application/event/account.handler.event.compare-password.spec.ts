5c1220b18881a48fcfb4e695c908aeaed642b3f8
kyhsa93 <kyhsa93@naver.com>
2019-09-22 18:57:19 +0900

    Add event handler spec

---
+++ src/account/application/event/account.handler.event.compare-password.spec.ts
@@ -0,0 +1,28 @@
+import { TestingModule, Test } from "@nestjs/testing";
+import { CqrsModule } from "@nestjs/cqrs";
+import { ComparePasswordEventHandler } from './account.handler.event.compare-password';
+import { ComparePasswordEvent } from "./account.event.compare-password";
+
+describe('ComparePasswordEventHandler', () => {
+  let module: TestingModule;
+  let comparePasswordEventHandler: ComparePasswordEventHandler;
+  let comparePasswordEvent: ComparePasswordEvent;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [CqrsModule],
+      providers: [ComparePasswordEventHandler],
+    }).compile();
+    comparePasswordEventHandler = module.get(ComparePasswordEventHandler);
+  });
+
+  afterAll(async () => close());
+
+  describe('handle', () => {
+    comparePasswordEvent = new ComparePasswordEvent('accountId');
+    it('run event handler', () => {
+      const result = comparePasswordEventHandler.handle(comparePasswordEvent);
+      expect(result).toBe(undefined);
+    });
+  });
+});
