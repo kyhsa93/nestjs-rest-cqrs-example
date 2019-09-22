1be80897ba2902a578354b387404298a4839e726
kyhsa93 <kyhsa93@naver.com>
2019-09-22 15:31:09 +0900

    Add model spec

---
+++ src/account/domain/model/account.model.spec.ts
@@ -0,0 +1,22 @@
+import bcrypt from 'bcrypt-nodejs';
+import { Account } from './account.model';
+import { ComparePasswordEvent } from '../../application/event/account.event.compare-password';
+
+describe('AccountModel', () => {
+  let accountModel: Account;
+  let comparePasswordEvent: ComparePasswordEvent;
+
+  afterAll(async () => close());
+
+  describe('comparePassword', () => {
+    accountModel = new Account('accountId', 'accountName', 'accountEmail', 'accountPassword', true);
+    comparePasswordEvent = new ComparePasswordEvent(accountModel.accountId);
+
+    it('execute comparePassword method', () => {
+      jest.spyOn(bcrypt, 'compareSync').mockImplementation(() => true);
+      jest.spyOn(accountModel, 'apply').mockImplementation(() => true);
+      accountModel.comparePassword('password');
+      expect(accountModel.apply).toBeCalledWith(comparePasswordEvent);
+    });
+  });
+});
