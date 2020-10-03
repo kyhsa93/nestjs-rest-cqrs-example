6b83ff97e43225f3598f42cde91a08d77bea4002
Young hoon Go <kyhsa93@naver.com>
2020-10-03 21:39:23 +0900

    Add AppService in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import AppController from '@src/app.controller';
+import AppService from '@src/app.service';
 
 import AuthModule from '@src/auth/auth.module';
 import AccountModule from '@src/account/account.module';
 
 @Module({
   imports: [AccountModule, AuthModule],
+  providers: [AppService],
   controllers: [AppController],
 })
 export default class ApplicationModule {}
