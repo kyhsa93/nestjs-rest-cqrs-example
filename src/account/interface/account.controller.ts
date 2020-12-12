3aa5279edda72e7ddd8962aa03b1cc619a4e8951
Young hoon Go <kyhsa93@naver.com>
2020-12-12 20:35:27 +0900

    fix: remove user dto from controller

--- src/account/interface/account.controller.ts
+++ src/account/interface/account.controller.ts
@@ -7,13 +7,10 @@ import {
   Param,
   Put,
   Delete,
-  Request,
-  UnauthorizedException,
   NotFoundException,
 } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 
-import UserDTO from '@src/account/interface/dto/user.dto';
 import CreateAccountBody from '@src/account/interface/dto/create.account.body';
 import UpdateAccountPathParam from '@src/account/interface/dto/update.account.param';
 import UpdateAccountBody from '@src/account/interface/dto/update.account.body';
@@ -40,32 +37,26 @@ export default class AccountController {
 
   @Put(':id')
   public async handleUpdateAccountRequest(
-    @Request() req: { user: UserDTO },
     @Param() param: UpdateAccountPathParam,
     @Body() body: UpdateAccountBody,
   ): Promise<void> {
-    if (param.id !== req.user.id) throw new UnauthorizedException();
     const { oldPassword, newPassword } = body;
     await this.commandBus.execute(new UpdateAccountCommand(param.id, oldPassword, newPassword));
   }
 
   @Delete(':id')
   public async handleDeleteAccountRequest(
-    @Request() req: { user: UserDTO },
     @Param() param: DeleteAccountPathParam,
     @Body() body: DeleteAccountBody,
   ): Promise<void> {
-    if (param.id !== req.user.id) throw new UnauthorizedException();
     const { password } = body;
     await this.commandBus.execute(new DeleteAccountCommand(param.id, password));
   }
 
   @Get(':id')
   public async handlerGetAccountByIdRequest(
-    @Request() req: { user: UserDTO },
     @Param() param: ReadAccountPathParam,
   ): Promise<Account> {
-    if (param.id !== req.user.id) throw new UnauthorizedException();
     const account: Account = await this.queryBus.execute(new ReadAccountQuery(param.id));
     if (!account) throw new NotFoundException();
     return account;
--- src/account/interface/dto/user.dto.ts
+++
@@ -1,3 +0,0 @@
-export default class UserDTO {
-  constructor(public readonly id: string, public readonly email: string) {}
-}
