0dacad2e10354c412848835bf9b26a8afce23eb2
Young hoon Go <kyhsa93@naver.com>
2021-04-25 17:44:27 +0900

    test: add account spec

---
+++ src/accounts/domain/account.spec.ts
@@ -0,0 +1,22 @@
+import { Account, AccountAttributes } from "src/accounts/domain/account";
+
+describe('Account', () => {
+  describe('attributes', () => {
+    it('should return AccountAttributes', () => {
+      const accountAttributes: AccountAttributes = {
+        id: 'accountId',
+        name: 'tester',
+        password: 'password',
+        balance: 0,
+        openedAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      const account = new Account(accountAttributes);
+
+      const result = account.attributes();
+
+      expect(result).toEqual(accountAttributes);
+    });
+  });
+});
