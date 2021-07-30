4d03c6d5881a7b96b9dbbe0f782702752c72d2b7
Young hoon Go <kyhsa93@naver.com>
2021-07-30 19:20:27 +0900

    test: add event handler spec

---
+++ src/accounts/application/events/account-closed.handler.spec.ts
@@ -0,0 +1,67 @@
+import { Logger, ModuleMetadata, Provider } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+
+import { AccountClosedHandler } from "src/accounts/application/events/account-closed.handler";
+import { EventStore, IntegrationEventPublisher, IntegrationEventSubject } from "src/accounts/application/events/integration";
+import { InjectionToken } from "src/accounts/application/injection.token";
+
+import { AccountClosedEvent } from "src/accounts/domain/events/account-closed.event";
+
+describe('AccountClosedHandler', () => {
+  let handler: AccountClosedHandler;
+  let logger: Logger;
+  let publisher: IntegrationEventPublisher;
+  let store: EventStore;
+
+  beforeEach(async () => {
+    const loggerProvider: Provider = {
+      provide: Logger,
+      useValue: {},
+    };
+    const publisherProvider: Provider = {
+      provide: InjectionToken.INTEGRATION_EVENT_PUBLISHER,
+      useValue: {},
+    };
+    const storeProvider: Provider = {
+      provide: InjectionToken.EVENT_STORE,
+      useValue: {},
+    };
+    const providers: Provider[] = [
+      AccountClosedHandler,
+      loggerProvider,
+      publisherProvider,
+      storeProvider,
+    ];
+    const moduleMetadata: ModuleMetadata = { providers };
+    const testModule = await Test.createTestingModule(moduleMetadata).compile();
+
+    handler = testModule.get(AccountClosedHandler);
+    logger = testModule.get(Logger);
+    publisher = testModule.get(InjectionToken.INTEGRATION_EVENT_PUBLISHER);
+    store = testModule.get(InjectionToken.EVENT_STORE);
+  });
+
+  describe('handle', () => {
+    it('should handle AccountClosedEvent', async () => {
+      logger.log = jest.fn();
+      publisher.publish = jest.fn();
+      store.save = jest.fn();
+
+      const event = { id: 'accountId' } as AccountClosedEvent;
+
+      await expect(handler.handle(event)).resolves.toEqual(undefined);
+      expect(logger.log).toBeCalledTimes(1);
+      expect(logger.log).toBeCalledWith(`${IntegrationEventSubject.CLOSED}: ${JSON.stringify(event)}`);
+      expect(publisher.publish).toBeCalledTimes(1);
+      expect(publisher.publish).toBeCalledWith({
+        subject: IntegrationEventSubject.CLOSED,
+        data: { id: event.id },
+      });
+      expect(store.save).toBeCalledTimes(1);
+      expect(store.save).toBeCalledWith({
+        subject: IntegrationEventSubject.CLOSED,
+        data: event,
+      })
+    })
+  })
+});
